c3a90d2323e819ec8391f9f454ff1dbc16f9664f
Anthony Dorfman <antdorf@gmail.com>
2025-03-22 14:37:14 -0700

    v2 testing WIP

--- v2/v2.go
+++ v2/v2.go
@@ -1,17 +1,18 @@
 package v2
 
 import (
-	//	"errors"
-	//	"fmt"
-	//	"io"
-	//	"os"
-	//	"os/exec"
-	//	"strings"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"os/exec"
+	"strings"
 
 	"github.com/goccy/go-yaml"
 )
 
-type Var struct {
+type VarCfg struct {
 	FromCommand string `yaml:"from_command"`
 	FromEnv     string `yaml:"from_env"`
 	Default     string `yaml:"default"`
@@ -45,24 +46,24 @@ type DexFile2 struct {
 */
 func Run(dexFile DexFile2, args []string) {
 
-	// /* No commands asked for: show menu and exit */
-	//
-	//	if len(args) == 1 {
-	//		displayMenu(os.Stdout, dexFile, 0)
-	//		os.Exit(0)
-	//	}
+	/* No commands asked for: show menu and exit */
+
+	if len(args) == 1 {
+		displayMenu(os.Stdout, dexFile.Blocks, 0)
+		os.Exit(0)
+	}
 	//
 	// /* No commands were found from the arguments the user passed: show error, menu and exit */
-	// commands, err := resolveCmdToCodeblock(dexFile, args[1:])
+	commands, err := resolveCmdToCodeblock(dexFile.Blocks, args[1:])
 	//
-	//	if err != nil {
-	//		fmt.Fprintf(os.Stderr, "Error: No commands were found at %v\n\nSee the menu:\n", args[1:])
-	//		displayMenu(os.Stderr, dexFile, 0)
-	//		os.Exit(1)
-	//	}
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: No commands were found at %v\n\nSee the menu:\n", args[1:])
+		displayMenu(os.Stderr, dexFile.Blocks, 0)
+		os.Exit(1)
+	}
 	//
 	// /* Found commands: run them */
-	// runCommands(commands)
+	runCommands(commands)
 }
 
 /*
@@ -74,7 +75,67 @@ func ParseConfig(configData []byte) (DexFile2, error) {
 
 	if err := yaml.Unmarshal([]byte(configData), &dexFile); err != nil {
 		return DexFile2{}, err
+	} else if dexFile.Version != 2 {
+		return DexFile2{}, errors.New("incorrect version number")
 	}
 
 	return dexFile, nil
+}
+
+/*
+Display the menu by recursively processing each element of the DexFile and
+
+	showing the name and description for the command.  Children are indented with
+	4 spaces.
+*/
+func displayMenu(w io.Writer, blocks []Block, indent int) {
+	for _, elem := range blocks {
+
+		fmt.Fprintf(w, "%s%-24v: %v\n", strings.Repeat(" ", indent*4), elem.Name, elem.Desc)
+
+		if len(elem.Children) >= 1 {
+			displayMenu(w, elem.Children, indent+1)
+		}
+	}
+}
+
+func resolveCmdToCodeblock(blocks []Block, cmds []string) ([]Command, error) {
+	for _, elem := range blocks {
+		if elem.Name == cmds[0] {
+			if len(cmds) >= 2 {
+				return resolveCmdToCodeblock(elem.Children, cmds[1:])
+			} else {
+				return elem.Commands, nil
+			}
+		}
+	}
+	return []Command{}, errors.New("could not find command")
+}
+
+type CommandConfig struct {
+	Stdout io.Writer
+	Stderr io.Writer
+}
+
+func runCommands(commands []Command) {
+
+	config := CommandConfig{
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+	}
+
+	runCommandsWithConfig(commands, config)
+}
+
+func runCommandsWithConfig(commands []Command, config CommandConfig) {
+	for _, command := range commands {
+		cmd := exec.Command("/bin/bash", "-c", command.Exec)
+		cmd.Stdout = config.Stdout
+		cmd.Stderr = config.Stderr
+
+		err := cmd.Run()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Failed to run command: ", err)
+		}
+	}
 }
---
+++ v2/v2_test.go
@@ -0,0 +1,114 @@
+package v2
+
+import (
+	//"bytes"
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func check(t *testing.T, e error, s string) {
+	if e != nil {
+		t.Errorf("%s - %v", s, e)
+	}
+}
+
+func createTestConfig(t *testing.T, config string) (*os.File, []byte, error) {
+
+	data := []byte(config)
+
+	tcfg, err := os.CreateTemp("", "dex-test")
+	check(t, err, "Error creating temp cfg file")
+
+	_, err = tcfg.Write(data)
+	check(t, err, "Error writing to temp cfg file")
+
+	yamlFile, err := os.Open(tcfg.Name())
+	check(t, err, "Error opening temp yaml file")
+
+	yamlData, err := io.ReadAll(yamlFile)
+	check(t, err, "Error reading yaml data")
+
+	return tcfg, yamlData, nil
+}
+
+type DexTest struct {
+	Name      string
+	Config    string
+	Dexfile   DexFile2
+	MenuOut   string
+	Blockpath []string
+	Commands  []string
+}
+
+func TestParseConfigFile(t *testing.T) {
+
+	tests := []DexTest{
+		{
+			Name: "Hello",
+			Config: `---
+- name: hello
+  desc: this is a command description`,
+			Dexfile: DexFile2{
+				Blocks: []Block{
+					{
+						Name: "hello",
+						Desc: "this is a command description",
+					},
+				},
+			},
+		},
+		{
+			Name: "Hello Children",
+			Config: `---
+- name: hello
+  desc: this is a command description
+  children:
+    - name: start
+      desc: start the server
+    - name: stop
+      desc: stop the server
+    - name: restart
+      desc: restart the server
+`,
+			Dexfile: DexFile2{
+				Blocks: []Block{
+					{
+						Name: "hello",
+						Desc: "this is a command description",
+						Children: []Block{
+							{
+								Name: "start",
+								Desc: "start the server",
+							},
+							{
+								Name: "stop",
+								Desc: "stop the server",
+							},
+							{
+								Name: "restart",
+								Desc: "restart the server",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+
+		tcfg, yamlData, _ := createTestConfig(t, test.Config)
+
+		defer os.Remove(tcfg.Name())
+
+		dex_file, err := ParseConfig(yamlData)
+		check(t, err, "config file not found")
+
+		assert.Equal(t, dex_file, test.Dexfile)
+
+	}
+
+}
